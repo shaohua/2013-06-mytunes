4ad8e4861bc7fb21c2b5690796528bfc7ad047b4
Shaohua Zhou <sh.zhou@gmail.com>
2013-07-04 07:50:24 -0700

    Capture play event and add songs to songQueue

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ var App = Backbone.Model.extend({
 
     params.library.on('play', function(song){
       this.set('currentSong', song);
-      console.log('catch play event', song);
+      console.log('catch play event in App.js', song, this.get('songQueue').length);
+      this.get('songQueue').add(song);
     }, this);
 
     params.library.on('ended', function(song){
--- src/SongQueueEntryView.js
+++ src/SongQueueEntryView.js
@@ -1,3 +1,19 @@
 var SongQueueEntryView = Backbone.View.extend({
+  tagName: 'tr',
 
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+
+  initialize: function(){
+    
+  },
+
+  events: {
+    'click': function() {
+      this.model.play();
+    }
+  },
+
+  render: function(){
+    return this.$el.html(this.template(this.model.attributes));
+  }
 });
