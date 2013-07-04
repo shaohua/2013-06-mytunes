5d0f5ba32b55aafe307e6af1f0ce42b474681eb8
Shaohua Zhou <sh.zhou@gmail.com>
2013-07-04 11:13:15 -0700

    Put more logic inside AppView.js

--- src/AppView.js
+++ src/AppView.js
@@ -1,18 +1,25 @@
 var AppView = Backbone.View.extend({
 
   initialize: function(params){
+    _.bindAll(this, 'render');
     this.playerView = new PlayerView({model: this.model.get('currentSong')});
+    this.songQueueView = new SongQueueView({collection: this.model.get('songQueue')});
 
     this.model.on('change:currentSong', function(model){
       this.playerView.setSong(model.get('currentSong'));
     }, this);
-  },
+
+    this.model.get('songQueue').on('add', this.render);
+    this.model.get('songQueue').on('remove', this.render);
+
+  },//end initialize
 
   render: function(){
+    console.log('AppView.render()');
     return this.$el.html([
       this.playerView.$el,
       new LibraryView({collection: this.model.get('library')}).render(),
-      new SongQueueView({collection: this.model.get('songQueue')}).render()
+      this.songQueueView.render()
     ]);
   }
 
